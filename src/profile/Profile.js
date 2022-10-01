c04ce78ee4f5fc0cd47001f4f2cb36bfa4cb23c2
Mellef <mellef_med_ali@hotmail.fr>
2022-10-01 10:25:47 +0100

    adding inline style

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,29 +1,29 @@
-import bootstrap from 'bootstrap';
-import Button from 'bootstrap';
 import PropTypes from "prop-types";
 
 
-    export default function Profile(props) {
-console.log(props);
-const Fonction = () => props.handleFunction(props.fullName);
+export default function Profile(props) {
+    console.log(props);
+    const buttonStyle = { width: "120px", height: "60px", textAlign: "center", color: "white", borderRadius: "10px", backgroundColor: "grey", boxShadow: "3px 3px 3px blue" }
+    const Fonction = () => props.handleFunction(props.fullName);
     return (
         <>
             {props.children}
-            <button onClick={Fonction}>Submit</button>
+            <br />
+            <button style={buttonStyle} onClick={Fonction}>Submit</button>
         </>
     );
 }
 Profile.defaultProps = {
-    fullName:"Full name",
-    bio:"default biographie" ,
-    profession:" defaultprofession" 
+    fullName: "Full name",
+    bio: "default biographie",
+    profession: " defaultprofession"
 }
 
 Profile.propTypes = {
-    fullName : PropTypes.string,
-    bio : PropTypes.string,
-    profession : PropTypes.string,
-  }
+    fullName: PropTypes.string,
+    bio: PropTypes.string,
+    profession: PropTypes.string,
+}
 
 // The profile component gets data (fullName, bio, profession) as props, and gets image as children props, 
 // and the function "handleName" functions as a prop that sends an alert message with the name of the profile user.
